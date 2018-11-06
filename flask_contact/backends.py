94378d74dadb1afc3189439b73d480165293f4d6
David Dugue <ddugue@kumoweb.ca>
2018-11-06 11:19:57 -0500

    Added SMTP Backend

--- flask_contact/backends.py
+++ flask_contact/backends.py
@@ -1,6 +1,8 @@
 """ Module that regroups different email backends """
 import html
 import os
+import smtplib
+import textwrap
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
@@ -53,28 +55,12 @@ class EmailBackend:
             lines.append("%s: %s" % (k, item))
         return html.escape('\n'.join(lines))
 
-    def mail(self, fields, file=None):
-        """ Generate and send an email based on fields passed """
-        raise NotImplementedError()
-
-class SESEmailBackend(EmailBackend):
-    """ Provide an interface for SES to send email """
-
-    def __init__(self, *args, access_key=None, secret_key=None, **kwargs):
-        super().__init__(*args, **kwargs)
-        import boto3
-        self.client = boto3.client(
-            'ses',
-            aws_access_key_id=access_key or os.getenv('AWS_ACCESS_KEY_ID_EMAIL'),
-            aws_secret_access_key=secret_key or os.getenv('AWS_SECRET_ACCESS_KEY_EMAIL'),
-        )
-
     def get_reply_email(self, fields):
         """ Return a reply email to be included in email """
         return fields.get('email')
 
-    def mail(self, fields, file=None):
-        """ We use boto3 to send email """
+    def get_mail(self, fields, file=None):
+        """ Return a Mimetype message """
         message = MIMEMultipart()
         message['Subject'] = self.get_subject(fields)
         message['From'] = self.from_email
@@ -95,6 +81,81 @@ class SESEmailBackend(EmailBackend):
             part = MIMEApplication(attachment.read())
             part.add_header('Content-Disposition', 'attachment', filename=attachment.filename)
             message.attach(part)
+
+        return message
+
+    def mail(self, fields, file=None):
+        """ Generate and send an email based on fields passed """
+        raise NotImplementedError()
+
+class SMTPEmailBackend(EmailBackend):
+    """ Provide an SMTP interface to send email """
+
+    MISSING_SERVER = textwrap.dedent("""
+    You need to provide 'smtp_server' value (normally smtp.yourdomain.com) for
+    the SMTPEmailBackend to work. """)
+    MISSING_USER = textwrap.dedent("""
+    You need to provide 'smtp_user' value (normally user@yourdomain.com) for
+    the SMTPEmailBackend to work. """)
+    MISSING_PASS = textwrap.dedent("""
+    You need to provide 'smtp_password' or environment variable SMTP_PASSWORD
+    for the SMTPEmailBackend to work. """)
+
+    def __init__(self, *args, smtp_server=None, smtp_user=None,
+                 smtp_password=None, smtp_port=465, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.smtp_server   = smtp_server
+        self.smtp_user     = smtp_user
+        self.smtp_password = smtp_password or os.getenv('SMTP_PASSWORD')
+        self.smtp_port     = smtp_port
+
+        assert self.smtp_server, self.MISSING_SERVER
+        assert self.smtp_user, self.MISSING_USER
+        assert self.smtp_password, self.MISSING_PASS
+
+
+    def mail(self, fields, file=None):
+        """ We use SMTP to send an email """
+        message = self.get_mail(fields, file=None)
+        try:
+            server = smtplib.SMTP_SSL(self.smtp_server, self.smtp_port)
+            server.login(self.smtp_user, self.smtp_password)
+            server.sendmail(message['From'], message['To'], message.as_string())
+            server.close()
+        except smtplib.SMTPAuthenticationError:
+            print("The username and/or password you entered is incorrect")
+
+class SESEmailBackend(EmailBackend):
+    """ Provide an interface for SES to send email """
+
+    MISSING_CREDENTIALS = textwrap.dedent("""
+    Please provide AWS valid email credentials for the SES Email backend to work
+    * You can provide the access key via the access_key parameter in the
+    SESEmailBackend constructor or via AWS_ACCESS_KEY_ID_EMAIL environment variable
+    * You can provide the secret key via the secret_key parameter in the
+    SESEmailBackend constructor or via AWS_SECRET_ACCESS_KEY_EMAIL environment variable
+    """)
+
+    def __init__(self, *args, access_key=None, secret_key=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        import boto3
+        access_key = access_key or os.getenv('AWS_ACCESS_KEY_ID_EMAIL')
+        secret_key = secret_key or os.getenv('AWS_SECRET_ACCESS_KEY_EMAIL')
+
+        assert access_key, self.MISSING_CREDENTIALS
+        assert secret_key, self.MISSING_CREDENTIALS
+
+        self.client = boto3.client(
+            'ses',
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key or os.getenv('AWS_SECRET_ACCESS_KEY_EMAIL'),
+        )
+
+
+    def mail(self, fields, file=None):
+        """ We use boto3 to send email """
+        message = self.get_mail(fields, file)
 
         # We need to send raw email, otherwise file is not supported
         self.client.send_raw_email(
