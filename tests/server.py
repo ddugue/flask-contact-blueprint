ee48e6a99e1ac9175c6e6a33784cd427df263715
David Dugue <ddugue@kumoweb.ca>
2018-11-23 16:24:00 -0500

    Made test server more generic

--- tests/server.py
+++ tests/server.py
@@ -1,26 +1,21 @@
+""" This is a test server to run the test """
 from flask import Flask
 from flask_contact.backends import SESEmailBackend, SMTPEmailBackend
 from flask_contact import blueprint
 
 app = Flask(__name__)
 
-# app.register_blueprint(blueprint('contact', SESEmailBackend(
-#     'noreply@neosmartblinds.com',
-#     'ddugue@neosmartblinds.com',
-#     allowed_fields='email message'
-# )), url_prefix='/contact')
-
 # app.register_blueprint(blueprint('job', SESEmailBackend(
-#     'noreply@neosmartblinds.com',
-#     'ddugue@neosmartblinds.com',
+#     'noreply@example.com',
+#     'target@example.com',
 #     allowed_fields='email position message',
 #     allow_file=True,
 # )), url_prefix='/jobs')
 
 app.register_blueprint(blueprint('smtp', SMTPEmailBackend(
-    'ddugue@neosmartblinds.com',
-    'ddugue@kumoweb.ca',
-    smtp_user='ddugue@neosmartblinds.com',
+    'from@example.com',
+    'to@example.ca',
+    smtp_user='from@example.com',
     smtp_server='smtp.gmail.com',
     allowed_fields='email message',
     allow_file=True,
